5615175b1fdd2f4091983986eb9b3060b48a78fa
Mari-jean Joron <jmarijean@gmail.com>
2023-07-04 03:09:50 +0200

    bouton spoiler js

--- exo.js
+++ exo.js
@@ -1,18 +1,21 @@
+//declarer les variable
 let hidden = true; // Variable pour suivre l'état du message (caché ou affiché)
+let btn    = document.querySelector('button');
+let p      = document.querySelector('p');
 
-function toggleSpoiler() {
-    let messageElement = document.getElementById('message');
-    let spoilerButton = document.getElementById('spoilerButton');
+//cacher le message
+p.style.display= "none";
 
+//detecter le clic
+btn.addEventListener('click',()=>{
     if (hidden){
-        messageElement.classList.remove('hidden');// Supprime la classe "hidden" pour afficher le message
-        spoilerButton.textContent ="Cacher";// Met à jour le texte du bouton
-        hidden = false;// Met à jour l'état du message à affiché
+        btn.textContent="Cacher";
+        p.style.display="block";
+        hidden=false;
     }else{
-        messageElement.classList.add('hidden');// Ajoute la classe "hidden" pour cacher le message
-        spoilerButton.textContent = "Afficher";// Met à jour le texte du bouton
-        hidden = true;// Met à jour l'état du message à caché
+        btn.textContent="Afficher";
+        p.style.display="none";
+        hidden=true;
     }
-}
-let spoilerButton = document.getElementById('spoilerButton');
-spoilerButton.addEventListener('click', toggleSpoiler);
+
+})
